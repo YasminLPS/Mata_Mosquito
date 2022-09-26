1ce56df8ea0a287dfbce64d4a39c87cb494a57da
Yasmin Lopes <yasminqalopes54@gmail.com>
2022-09-26 14:21:50 -0300

    Posicao aleatoria

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var altura = 0;
+var largura= 0;
+function ajustaTamanho(){
+    altura  = window.innerHeight;
+    largura = window.innerWidth;
+    console.log(largura, altura);
+}
+
+ajustaTamanho();
+
+function posicaoAleatoria(){
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    if(posicaoX < 0 ){
+        posicaoX = 0;
+    }
+    
+    if(posicaoY < 0 ){
+        posicaoY = 0;
+    }
+
+    console.log(posicaoX, posicaoY)
+
+
+    //criar elemento HTML
+    var mosquito = document.createElement('img');
+    mosquito.src = 'imagens/mosca.png';
+    mosquito.className = 'mosquito1';
+    mosquito.style.left = posicaoX + 'px';
+    mosquito.style.top = posicaoY + 'px';
+    mosquito.style.position = 'absolute';
+    document.body.appendChild(mosquito);
+}
